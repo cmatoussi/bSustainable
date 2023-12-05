56036a8bb95822a95f96a5ccd213df75f6c35041
Syrine Matoussi <cyrne.matoussi@gmail.com>
2023-12-04 23:39:45 -0500

    added bar charts and icons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { useState } from 'react';
+import { GiTreeBranch, GiPathDistance } from "react-icons/gi";
+import { FaPlane, FaCar, FaBus } from "react-icons/fa";
+import { RiMotorbikeFill } from "react-icons/ri";
+import { MdDirectionsRailway } from "react-icons/md";
+import { FaFerry } from "react-icons/fa6";
+import BarChart from "./components/BarChart"; 
+import { FaTree } from 'react-icons/fa';
+import { FcGlobe } from "react-icons/fc";
 import axios from 'axios';
 
+
+
+
 function App() {
-  const travelMeans = [
-    { value: 'CarbonFootprintFromFlight', label: "Plane" },
-    { value: 'CarbonFootprintFromCarTravel', label: "Car" },
-    { value: 'CarbonFootprintFromMotorBike', label: "Motor Bike" },
-    { value: 'CarbonFootprintFromPublicTransit', label: "Public Transit" }
-  ];
 
   const flightTypes = [
     { value: "DomesticFlight", label: "Domestic Flight" },
@@ -25,16 +30,21 @@ function App() {
     { value: "LargeMotorBike", label: "Large Motor Bike" }
   ]; 
 
-  const publicTransitTypes = [
-    { value: "Taxi", label: "Taxi" },
+  const busTypes = [
     { value: "ClassicBus", label: "Classic Bus" },
-    { value: "Coach", label: "Coach" },
-    { value: "NationalTrain", label: "National Train" },
-    { value: "LightRail", label: "Light Rail" },
-    { value: "Subway", label: "Subway" },
+    { value: "Coach", label: "Coach" }
+  ];
+
+  const ferryTypes = [
     { value: "FerryOnFoot", label: "Ferry On Foot" },
     { value: "FerryInCar", label: "Ferry In Car" }
-  ]; 
+  ];
+
+  const railwayTypes = [
+    { value: "NationalTrain", label: "National Train" },
+    { value: "LightRail", label: "Light Rail" },
+    { value: "Subway", label: "Subway" }
+  ];
 
   const carTypes = [
     { value: "SmallDieselCar", label: "Small Diesel Car" },
@@ -55,9 +65,22 @@ function App() {
     { value: "CNGVan", label: "CNG Van" }, 
     { value: "SmallPetrolCar", label: "Small Petrol Car" },
     { value: "MediumPetrolCar", label: "Medium Petrol Car" },
-    { value: "LargePetrolCar", label: "Large Petrol Car" }, 
+    { value: "LargePetrolCar", label: "Large Petrol Car" }
 
   ]; 
+  const apiKey = 'f0971ecee8msh7fc7b4f891eb1e7p190eeajsn3fe1ee4d469d';
+
+  const [distance, setDistance] = useState(0);
+  const [travelMean, setTravelMean] = useState('CarbonFootprintFromFlight');
+  const [travelMode, setTravelMode] = useState('plane');
+  const [userEmissions, setUserEmissions] = useState(0);
+  const [vehicleType, setVehicleType] = useState('DomesticFlight');
+  const [publicTransitTypes, setPublicTransit] = useState('');
+  const [averageEmissions, setAverageEmissions] = useState(0);
+  const [hasBeenClicked, setHasBeenClicked] = useState(false);
+
+
+
   const travelVehicleMap = {
     'CarbonFootprintFromFlight': flightTypes,
     'CarbonFootprintFromCarTravel': carTypes,  // Assuming car doesn't have specific types in this example
@@ -65,20 +88,56 @@ function App() {
     'CarbonFootprintFromPublicTransit': publicTransitTypes  // Assuming public transit doesn't have specific types in this example
   };
 
-  const [distance, setDistance] = useState(0);
-  const [travelMean, setTravelMean] = useState('CarbonFootprintFromFlight');
-  const [co2Emissions, setCo2Emissions] = useState(0);
-  const [vehicleType, setVehicleType] = useState('');
+  const averagesEmission = {
+    'plane': 0.34, 
+    'car': 0.47,
+    'bus': 0.39,
+    'motorbike': 0.4,
+    'railway': 0.3,
+    'ferry': 0.4,
+  };
 
+  const handleIconClick = (input) => {
+    setTravelMode(input)
+    switch (input) {
+      case 'plane':
+        setTravelMean('CarbonFootprintFromFlight');
+        break;
+      case 'car':
+        setTravelMean('CarbonFootprintFromCarTravel');
+        break;
+      case 'bus':
+        setTravelMean('CarbonFootprintFromPublicTransit');
+        setPublicTransit(busTypes)
+        break;
+      case 'motorbike':
+        setTravelMean('CarbonFootprintFromMotorBike');
+        break;
+      case 'railway':
+        setTravelMean('CarbonFootprintFromPublicTransit');
+        setPublicTransit(railwayTypes)
+        break;
+      case 'ferry':
+        setTravelMean('CarbonFootprintFromPublicTransit');
+        setPublicTransit(ferryTypes)
 
-  const apiKey = '319e39cbbemsh0f75dee223acb16p1a21aejsnaf14af559a86';
+        break;
+      default:
+        setTravelMean('CarbonFootprintFromFlight');
+        break;
+    }
+  };
 
   const handleCalculateEmissions = async () => {
-    const url = 'https://carbonfootprint1.p.rapidapi.com/'+travelMean
+    const url = 'https://carbonfootprint1.p.rapidapi.com/' + travelMean;
+    setAverageEmissions(distance * averagesEmission[travelMode]);
+    const distanceInKilometers = Math.ceil(distance * 1.60934);
+    setHasBeenClicked(true);
+  
     try {
       const response = await axios.get(url, {
         params: {
-          distance: distance,
+          distance: distanceInKilometers,
           type: vehicleType,
         },
         headers: {
@@ -86,51 +145,136 @@ function App() {
           'X-RapidAPI-Host': 'carbonfootprint1.p.rapidapi.com',
         },
       });
-
-      setCo2Emissions(response.data.carbonEquivalent); // Access the 'carbonEquivalent' property
+      const kgEmissions = response.data.carbonEquivalent;
+      const lbsEmissions = Math.round(kgEmissions * 2.205);
+      setUserEmissions(lbsEmissions);
     } catch (error) {
       console.error(error);
     }
   };
+  
 
   return (
     <div>
-      <h1>CO2 Emissions Calculator</h1>
-      <form> 
-        <label>
-          Distance (KM):
-          <input type="number" value={distance} onChange={(e) => setDistance(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Travel Mean:
-          <select value={travelMean} onChange={(e) => setTravelMean(e.target.value)}>
-          {travelMeans.map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </select>
-        </label>
-        <br />
-        <label>
-          Vehicle Type:
-          <select value={vehicleType} onChange={(e) => setVehicleType(e.target.value)}>
-          {travelVehicleMap[travelMean].map((option) => (
-              <option key={option.value} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </select>
-        </label>
-        <br />
-        <button type="button" onClick={handleCalculateEmissions}>
-          Calculate CO2 Emissions
-        </button>
-      </form>
-        <p>
-          CO2 emissions for {distance} miles of {travelMean}: {co2Emissions} kg CO2
-        </p>
+      <div className="navbar">
+          <div className="logo">
+            <GiTreeBranch  color="green" size="35px"/>
+            <p>Bsustainable</p>
+          </div>
+          <nav>
+            <ul className="nav-links">
+              <li>
+                <a href="#home">Home</a>
+              </li>
+            </ul>
+          </nav>
+      </div>
+      <div className="page-container">
+        <div className="page-content">
+        <form> 
+          <br />
+          <div className="container">
+          <h1 style={{color: 'rgb(14, 123, 99)'}}>CO2 Emissions Calculator</h1>
+
+            <div className="mode-container">
+              <label> Travel Mean:</label>
+                <div className="icon-container">
+                    <FaPlane
+                      size="35px"
+                      color= "black"
+                      style={{ cursor: 'pointer' }}
+                      onClick={() => handleIconClick('plane')}
+                    />
+                    <FaCar
+                      size="35px"
+                      color= "darkblue"
+                      style={{ cursor: 'pointer' }}
+                      onClick={() => handleIconClick('car')}
+                    />
+                    <FaBus
+                      size="35px"
+                      color="goldenrod"
+                      style={{ cursor: 'pointer' }}
+                      onClick={() => handleIconClick('bus')}
+                    />
+                    <RiMotorbikeFill
+                      size="35px"
+                      color= "darkred"
+                      style={{ cursor: 'pointer' }}
+                      onClick={() => handleIconClick('motorbike')}
+                    />
+                    <MdDirectionsRailway
+                      size="35px"
+                      color="darkgreen"
+                      style={{ cursor: 'pointer' }}
+                      onClick={() => handleIconClick('railway')}
+                    />
+                    <FaFerry
+                      size="35px"
+                      color="darkorange"
+                      style={{ cursor: 'pointer' }}
+                      onClick={() => handleIconClick('ferry')}
+                    />
+                </div>
+              </div>
+          <br />
+            <label>
+            <p style={{ display: 'inline-block', margin: '10px' }}>Vehicle Type:</p>
+              <select value={vehicleType} onChange={(e) => setVehicleType(e.target.value)} style={{ paddingLeft: 'right' }}>
+              {travelVehicleMap[travelMean].map((option) => (
+                  <option key={option.value} value={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </label>
+          <br />
+
+            <label>
+            <p style={{ display: 'inline-block', margin: '10px' }}>Distance(mi): </p>
+              <input type="number" value={distance} onChange={(e) => setDistance(e.target.value)}  />
+            </label>
+          <br />
+          <button type="button" onClick={handleCalculateEmissions}>
+            Calculate CO2 Emissions
+          </button>
+          </div>
+
+        </form>
+
+        </div>
+        {hasBeenClicked ? (
+          <div style={{ width: '50%', margin: 'auto' }}>
+            <h3>Your Emissions Compared to your transport mode average</h3>
+            <BarChart averageEmissions={averageEmissions} userEmissions={userEmissions} />
+          </div>
+        ) : (
+          <div style={{ width: '50%', margin: 'auto' }}>
+         <FcGlobe  size= "60%"/>
+         </div>
+        )}
+        </div>
+
+      {hasBeenClicked && (
+        <div>
+          <h2>
+            Your trip produces {userEmissions} lbs of CO2   
+          </h2>
+          {userEmissions <= averageEmissions ? (
+              <h2 style={{ color: 'darkblue' }}>
+                You are emitting less CO2 than the average for your transport mode. Keep being sustainable! 👏💅
+              </h2>
+                ) : (
+              <h2 style={{ color: 'darkblue' }}>
+                You are emitting more CO2 than the average for your transport mode. Try to find more sustainable travel modes! 😞
+              </h2>
+        )}          
+        <h2 style ={{color:'darkblue'}}>
+        Fun Fact: If you plant {Math.ceil(userEmissions / 48)} <FaTree color="darkgreen" /> trees today, in a year you would have offset your emission from this trip!
+        </h2>
+        </div>
+      )}
+
     </div>
   );
 }
---
+++ src/components/BarChart.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useRef } from 'react';
+import Chart from 'chart.js/auto';
+
+const BarChart = ({ averageEmissions, userEmissions }) => {
+  const chartRef = useRef(null);
+  const chartInstance = useRef(null);
+
+  useEffect(() => {
+    // Destroy the previous chart instance before creating a new one
+    if (chartInstance.current) {
+      chartInstance.current.destroy();
+    }
+
+    if (chartRef && chartRef.current) {
+      const ctx = chartRef.current.getContext('2d');
+
+      chartInstance.current = new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: ['Average Footprint', 'Your Footprint'],
+          datasets: [
+            {
+              label: 'CO2 Emissions',
+              data: [averageEmissions, userEmissions],
+              backgroundColor: [
+                'blue', 
+                userEmissions < averageEmissions ? 'green' : 'red', 
+              ],
+            },
+          ],
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true,
+              grid: {
+                display: false, 
+              },
+            },
+            x: {
+              grid: {
+                display: false, 
+              },
+            },
+          },
+        },
+      });
+    }
+
+    // Return a cleanup function to destroy the chart when the component unmounts
+    return () => {
+      if (chartInstance.current) {
+        chartInstance.current.destroy();
+      }
+    };
+  }, [averageEmissions, userEmissions]);
+
+  return <canvas ref={chartRef} />;
+};
+
+export default BarChart;
